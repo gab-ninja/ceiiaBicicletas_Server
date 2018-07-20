90bbecf012ca4e44603000fdf3ba5a653e42dc95
mariogabrielcampos <mairiogabrielcampos@gmail.com>
2018-07-20 09:43:11 +0100

    mobi dock communication

--- routes/mobiDockRoutes.js
+++ routes/mobiDockRoutes.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const _ = require('lodash');
+const axios = require('axios');
 require('colors');
 
 module.exports = app => {
 	//const Dock = mongoose.model('docks');
 
-	app.post('/mobidock/position', (req, res) => {
+	app.post('/mobidock/position', async (req, res) => {
 		console.log('[MobiDock] '.yellow + `Recived position  ${JSON.stringify(req.body)}`);
+		try {
+			const response = await axios.get('http://192.168.1.153/api/1234567', 'ghdsbh');
+			console.log('[MobiDock] '.yellow + response);
+		} catch (err) {
+			console.log(err);
+		}
 		res.send('Position recived :)');
 	});
 };
