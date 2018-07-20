6eef7c6630ac3ed9dfe8e50ddf6cd474148d6cd4
mariogabrielcampos <mairiogabrielcampos@gmail.com>
2018-07-20 15:42:46 +0100

    minor bugs fixed

--- routes/mobiDockRoutes.js
+++ routes/mobiDockRoutes.js
@@ -8,8 +8,26 @@ module.exports = app => {
 
 	app.post('/mobidock/position', async (req, res) => {
 		console.log('[MobiDock] '.yellow + `Recived position  ${JSON.stringify(req.body)}`);
+		var docks = 'null';
 		try {
-			const response = await axios.get('http://192.168.1.153/api/1234567', 'ghdsbh');
+			docks = await Dock.find({});
+			docks = _.map(docks, dock => {
+				return {
+					_id: dock._id,
+					name: dock.name,
+					slots: dock.slots,
+					numberOfBikes: dock.availableBicycles.length,
+					location: {
+						latitude: dock.location.coordinates[1],
+						longitude: dock.location.coordinates[0]
+					}
+				};
+			});
+		} catch (error) {
+			console.log('[API] '.yellow + 'ERROR on fetching dock positions');
+		}
+		try {
+			const response = await axios.post('http://192.168.1.153/api/1234567', docks);
 			console.log('[MobiDock] '.yellow + response);
 		} catch (err) {
 			console.log(err);
